47150311389ef26c98a4716dd11e2f2eee40025b
madjack <kavanokami@gmail.com>
2020-02-04 15:34:15 +0400

    feat: add basic components

---
+++ src/components/hoc/with-bookstore-service.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BookstoreServiceConsumer } from '../bookstore-service-context';
+
+const withBookstoreService = WrappedComponent => {
+  return props => (
+    <BookstoreServiceConsumer>
+      {bookstoreService => {
+        return (
+          <WrappedComponent {...props} bookstoreService={bookstoreService} />
+        );
+      }}
+    </BookstoreServiceConsumer>
+  );
+};
+
+export default withBookstoreService;
