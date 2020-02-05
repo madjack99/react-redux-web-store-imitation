8ec94a4b85ecaeb59ee3c1a84f14ae67d93ba8f7
madjack <kavanokami@gmail.com>
2020-02-05 18:22:06 +0400

    feat: add booksRequested action

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -4,13 +4,15 @@ import compose from '../../utils';
 import BookListItem from '../book-list-item';
 import withBookstoreService from '../hoc';
 import Spinner from '../spinner';
-import { booksLoaded } from '../../actions';
+import { booksLoaded, booksRequested } from '../../actions';
 
 import './book-list.css';
 
 class BookList extends Component {
   componentDidMount() {
-    const { bookstoreService, booksLoaded } = this.props;
+    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+
+    booksRequested();
     bookstoreService
       .getBooks()
       .then(data => booksLoaded(data))
@@ -45,6 +47,7 @@ const mapStateToProps = ({ books, loading }) => {
 
 const mapDispatchToProps = {
   booksLoaded,
+  booksRequested,
 };
 
 export default compose(
