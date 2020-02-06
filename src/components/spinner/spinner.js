13d8bab85be4675eb501c6fccce7892f93bbd8d5
madjack <kavanokami@gmail.com>
2020-02-06 15:35:32 +0400

    feat: add spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import spinner from './infinity.png';
 
 import './spinner.css';
 
 const Spinner = () => {
-  return <div>Spinner</div>;
+  return (
+    <div className="spinner-container">
+      <img className="spinner" src={spinner} alt="spinner"></img>
+    </div>
+  );
 };
 
 export default Spinner;
