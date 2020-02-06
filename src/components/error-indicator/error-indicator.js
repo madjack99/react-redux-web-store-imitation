1c4d3885f2a3c365a30b53a3cbd4771413b9bbd6
madjack <kavanokami@gmail.com>
2020-02-06 15:31:51 +0400

    feat: add error

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import errorImage from './error-image.png';
 
 import './error-indicator.css';
 
 const ErrorIndicator = () => {
-  return <div>Error Indicator</div>;
+  return (
+    <div className="error-indicator">
+      <img className="error-indicator__image" src={errorImage} alt="error" />
+      <div className="error-indicator__text">
+        This error was initiated on purpose, please reload the page
+      </div>
+    </div>
+  );
 };
 
 export default ErrorIndicator;
